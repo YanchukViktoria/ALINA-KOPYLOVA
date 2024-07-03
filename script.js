882d301ee696a53f285790d88442fe67baa93e27
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-07-03 11:51:36 +0300

    anim

--- script.js
+++ script.js
@@ -1,53 +1,25 @@
-AOS.init();
-
-// (() => {
-//     function addClasses() {
-//         let ulElement = document.querySelector(".social-hidden-list");
-//         let pElement = document.querySelector(".footer-menu-desc");
-//         let svgElements = document.querySelectorAll(".footer-svg-anim");
-
-//         ulElement.classList.add("social-hidden-list-hidden");
-//         pElement.classList.add("footer-menu-desc-hidden");
-//         svgElements.classList.add("footer-svg-anim-hidden");
-//     }
-
-//     function removeClasses() {
-//         let ulElement = document.querySelector(".social-hidden-list");
-//         let pElement = document.querySelector(".footer-menu-desc");
-//         let svgElements = document.querySelectorAll(".footer-svg-anim");
-
-//         ulElement.classList.remove("social-hidden-list-hidden");
-//         pElement.classList.remove("footer-menu-desc-hidden");
-//         svgElements.classList.remove("footer-svg-anim-hidden");
-//     }
-
-//     let container = document.querySelector(".footer-anim-container");
-//     container.addEventListener("mouseover", addClasses);
-//     container.addEventListener("mouseout", removeClasses);
-// })();
-
 (() => {
-    function addClasses() {
-        let ulElement = document.querySelector(".social-hidden-list");
-        let pElement = document.querySelector(".footer-menu-desc");
-        let svgElement = document.querySelector(".footer-svg-anim");
-  
-        ulElement.classList.add("social-hidden-list-hidden");
-        pElement.classList.add("footer-menu-desc-hidden");
-        svgElement.classList.add("footer-svg-anim-hidden");
+    function addClasses(event) {
+        let container = event.currentTarget;
+        container.querySelector(".social-hidden-list")?.classList.add("social-hidden-list-hidden");
+        container.querySelector(".social-hidden-list-listen")?.classList.add("social-hidden-list-hidden");
+        container.querySelector(".footer-menu-desc")?.classList.add("footer-menu-desc-hidden");
+        container.querySelector(".footer-svg-anim")?.classList.add("footer-svg-anim-hidden");
+        container.querySelector(".footer-menu-desc-listen")?.classList.add("footer-menu-desc-listen-hidden");
     }
-  
-    function removeClasses() {
-        let ulElement = document.querySelector(".social-hidden-list");
-        let pElement = document.querySelector(".footer-menu-desc");
-        let svgElement = document.querySelector(".footer-svg-anim");
-  
-        ulElement.classList.remove("social-hidden-list-hidden");
-        pElement.classList.remove("footer-menu-desc-hidden");
-        svgElement.classList.remove("footer-svg-anim-hidden");
+
+    function removeClasses(event) {
+        let container = event.currentTarget;
+        container.querySelector(".social-hidden-list")?.classList.remove("social-hidden-list-hidden");
+        container.querySelector(".social-hidden-list-listen")?.classList.remove("social-hidden-list-hidden");
+        container.querySelector(".footer-menu-desc")?.classList.remove("footer-menu-desc-hidden");
+        container.querySelector(".footer-svg-anim")?.classList.remove("footer-svg-anim-hidden");
+        container.querySelector(".footer-menu-desc-listen")?.classList.remove("footer-menu-desc-listen-hidden");
     }
-  
-    let div = document.querySelector(".footer-anim-container");
-    div.addEventListener("mouseover", addClasses);
-    div.addEventListener("mouseout", removeClasses);
-  })();
+
+    let divs = document.querySelectorAll(".footer-anim-container");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", addClasses);
+        div.addEventListener("mouseout", removeClasses);
+    });
+})();

c73549a06c9a2304e7badb000fbebadb106727b3
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-07-12 12:08:34 +0300

    play

--- script.js
+++ script.js
@@ -23,3 +23,63 @@
         div.addEventListener("mouseout", removeClasses);
     });
 })();
+
+
+
+///
+
+
+let audio = document.getElementById('audio');
+let progressContainer = document.querySelector('.progress-container');
+let progress = document.getElementById('progress');
+let playPauseBtn = document.getElementById('playPauseBtn');
+let currentTimeSpan = document.getElementById('currentTime');
+let durationSpan = document.getElementById('duration');
+let ues = document.getElementById("audio-use");
+
+playPauseBtn.addEventListener("click", function () {
+    togglePlayPause();
+})
+
+audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('loadedmetadata', () => {
+    durationSpan.textContent = formatTime(audio.duration);
+});
+
+function togglePlayPause() {
+    if (audio.paused) {
+        audio.play();
+        ues.setAttribute('xlink:href', `#play`);
+    } else {
+        audio.pause();
+        ues.setAttribute('xlink:href', `#stop`);
+    }
+}
+
+function updateProgress() {
+    let { duration, currentTime } = audio;
+    let progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    currentTimeSpan.textContent = formatTime(currentTime);
+}
+
+function setProgress(event) {
+    let width = progressContainer.clientWidth;
+    let clickX = event.offsetX;
+    let duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+}
+
+
+
+
+

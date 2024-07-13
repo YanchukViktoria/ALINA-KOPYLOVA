bffc91e8ac10d8d920d8343c93164e984deb89f2
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-07-13 10:53:19 +0300

    burger

--- burger.js
+++ burger.js
@@ -1,14 +1,24 @@
+
+
 (() => {
-const refs = {
-openMenuBtn: document.querySelector('.menu-open-btn'),
-closeMenuBtn: document.querySelector('.menu-close-btn'),
-menu: document.querySelector('.mob-menu'),
-};
+    const refs = {
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mob-menu'),
+    li: document.querySelectorAll('.nav-menu-list-item-burger'),
+    };
+    
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    
+    function toggleMenu() {
+        refs.menu.classList.toggle('is-hidden');
+        
+        refs.li.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.toggle('animation');
+            }, index * 300);
+        });
+    }
+    })();
 
-refs.openMenuBtn.addEventListener('click', toggleMenu);
-refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-refs.menu.classList.toggle('is-hidden');
-}
-})();
